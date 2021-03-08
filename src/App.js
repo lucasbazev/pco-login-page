0f366195cb6add579d8f5586bcd85e962ebb1390
Lucas Azevedo <lucasbazev@gmail.com>
2021-03-08 12:05:39 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Planning Center</h1>
+      
+      <div className='form'>
+        <input placeholder='Your Email or Phone'/>
+        <input placeholder='Password'/>
+        <button>Log In</button>
+        <a href='/'>Need a password?</a>
+      </div>
     </div>
   );
 }
